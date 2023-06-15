48813550dfddcbba90ce315ec70d4cac84ddbeaa
Afride Hossain <54279218+AfrideHossain@users.noreply.github.com>
2023-06-15 23:48:55 +0600

    class deny route added

--- routes/manageClasses/manageClasses.js
+++ routes/manageClasses/manageClasses.js
@@ -5,6 +5,8 @@ const jwt = require("jsonwebtoken");
 const { client } = require("../../mongoDbConnection");
 const verifyJwt = require("../middlewares/verifyJwt");
 const instructorVerify = require("../middlewares/instructorVerify");
+const adminVerify = require("../middlewares/adminVerify");
+const { ObjectId } = require("mongodb");
 // mongo db collections
 const classes_collection = client.db("apertureacademy").collection("classes");
 
@@ -14,7 +16,7 @@ router.post("/addclass", verifyJwt, instructorVerify, async (req, res) => {
   let addClassReq = await classes_collection.insertOne(classInfo);
   res.send(addClassReq);
 });
-// route 1 : Add class
+// route 1 : Instructors classes
 router.get(
   "/instructorsclasses",
   verifyJwt,
@@ -26,5 +28,29 @@ router.get(
     res.send(classes);
   }
 );
+// route 2 : All classes
+router.get("/allclasses", verifyJwt, adminVerify, async (req, res) => {
+  let classes = await classes_collection.find().toArray();
+  res.send(classes);
+});
+// route 3 : Update class status to approve
+router.put("/class/approve/:id", verifyJwt, adminVerify, async (req, res) => {
+  const classId = req.params.id;
+  let approveClass = await classes_collection.updateOne(
+    { _id: new ObjectId(classId) },
+    { $set: { status: "approved" } }
+  );
+  res.send(approveClass);
+});
+// route 4 : Update class status to denied
+router.put("/class/deny/:id", verifyJwt, adminVerify, async (req, res) => {
+  const classId = req.params.id;
+  const feedback = req.body.feedback;
+  let denyClass = await classes_collection.updateOne(
+    { _id: new ObjectId(classId) },
+    { $set: { status: "denied", feedback: feedback } }
+  );
+  res.send(denyClass);
+});
 
 module.exports = router;
