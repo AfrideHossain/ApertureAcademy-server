da3aab34c2ef0093697c665b8e22d4a62c9ae463
Afride Hossain <54279218+AfrideHossain@users.noreply.github.com>
2023-06-16 15:15:54 +0600

    make instructor route added

--- routes/manageUsers/manageUsers.js
+++ routes/manageUsers/manageUsers.js
@@ -13,5 +13,18 @@ router.get("/users", verifyJwt, adminVerify, async (req, res) => {
   let users = await user_collections.find().toArray();
   res.send(users);
 });
-
+// route 2 : make instructor
+router.put(
+  "/user/makeInstructor/:id",
+  verifyJwt,
+  adminVerify,
+  async (req, res) => {
+    const userId = req.params.id;
+    let makeInstructor = await user_collections.updateOne(
+      { _id: new ObjectId(userId) },
+      { $set: { status: "approved" } }
+    );
+    res.send(makeInstructor);
+  }
+);
 module.exports = router;
